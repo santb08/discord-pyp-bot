d1b0c479346efbd984c0de31519a41296b68b1d6
Santiago <scb9908@gmail.com>
2022-09-09 01:46:28 -0500

    fix: typo

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const ENDPOINT_URL = process.env.ENDPOINT_URL;
 const MESSAGE_TEMPLATE =  'Hoy tienen pico y placa los vehículos particulares '
-  + 'con placas terminadas en ✨ **{0}**  ✨ y las las motos cuyas placas comienzan en ✨ **{1}**  ✨\n '
+  + 'con placas terminadas en ✨ **{0}**  ✨ y las motos cuyas placas comienzan en ✨ **{1}**  ✨\n '
   + '**Ojo pues papi que me lo cogen y me lo multan** 🥵 🥵 💯 💪';
 
 module.exports.getPyp = async () => {
